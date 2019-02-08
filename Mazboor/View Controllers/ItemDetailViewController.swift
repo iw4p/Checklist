484be0f2c07131fd1ab937d5972d11b6321e4d1a
Nima Akbarzade <Nimakbarzade@yahoo.com>
2019-02-09 01:26:04 +0330

    names changed

---
+++ Mazboor/View Controllers/ItemDetailViewController.swift
@@ -0,0 +1,284 @@
+//
+//  AddItemViewController.swift
+//  Mazboor
+//
+//  Created by Nima Akbarzade on 10/10/1397 AP.
+//  Copyright © 1397 AP Nima Akbarzade. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+protocol AddItemViewControllerDelegate: class {
+    func itemDetailViewControllerDidCancel(
+        _ controller: ItemDetailViewController)
+    func itemDetailViewController(
+        _ controller: ItemDetailViewController,
+        didFinishAdding item: ChecklistItem)
+    func itemDetailViewController(_ controller: ItemDetailViewController,
+                               didFinishEditing item: ChecklistItem)
+}
+
+class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
+
+    weak var delegate: AddItemViewControllerDelegate?
+    var itemToEdit: ChecklistItem?
+    var dueDate = Date()
+    var datePickerVisible = false
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    @IBOutlet weak var shouldRemindSwitch: UISwitch!
+    @IBOutlet weak var dueDateLabel: UILabel!
+    @IBOutlet weak var datePickerCell: UITableViewCell!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+        doneBarButton.isEnabled = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if #available(iOS 11.0, *) {
+            navigationItem.largeTitleDisplayMode = .never
+        } else {
+            // Fallback on earlier versions
+        }
+        shouldRemindSwitch.addTarget(self, action: #selector(ItemDetailViewController.switchStateDidChange(_:)), for: .valueChanged)
+
+        if let item = itemToEdit {
+            title = "Edit Item"
+            textField.text = item.text
+            doneBarButton.isEnabled = true
+            shouldRemindSwitch.isOn = item.shouldRemind
+            dueDate = item.dueDate
+        }
+        updateDueDateLabel()
+    }
+    
+    @objc func switchStateDidChange(_ sender:UISwitch){
+        if (sender.isOn == true){
+            doneBarButton.isEnabled = true
+        }
+        else{
+            doneBarButton.isEnabled = true
+        }
+    }
+    
+    func updateDueDateLabel() {
+        let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .persian)
+        formatter.locale = Locale(identifier: "En-IR")
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        dueDateLabel.text = formatter.string(from: dueDate)
+    }
+    
+    func showDatePicker() {
+        datePickerVisible = true
+        let indexPathDatePicker = IndexPath(row: 2, section: 1)
+        tableView.insertRows(at: [indexPathDatePicker], with: .fade)
+        datePicker.setDate(dueDate, animated: true)
+        dueDateLabel.textColor = dueDateLabel.tintColor
+        datePicker.datePickerMode = .dateAndTime
+        datePicker.locale = NSLocale.init(localeIdentifier: "En-IR") as Locale
+        datePicker.calendar = NSCalendar(calendarIdentifier: .persian)! as Calendar
+        datePicker.setValue(UIColor.white, forKeyPath: "textColor")
+        datePicker.setValue(false, forKeyPath: "highlightsToday")
+
+    }
+    
+    func hideDatePicker() {
+        if datePickerVisible {
+            datePickerVisible = false
+            let indexPathDatePicker = IndexPath(row: 2, section: 1)
+            tableView.deleteRows(at: [indexPathDatePicker], with: .fade)
+            dueDateLabel.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
+    @IBAction func dateChanged(_ datePicker: UIDatePicker) {
+        dueDate = datePicker.date
+        updateDueDateLabel()
+        if textField.text == "" {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+    }
+    
+    // MARK:- Text Field Delegates
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange,with: string)
+        
+        if newText.isEmpty {
+      
+            doneBarButton.isEnabled = false
+     
+        } else {
+      
+            doneBarButton.isEnabled = true
+        }
+      
+        return true
+    }
+
+    
+    // MARK:- Actions
+    @IBAction func cancel() {
+        delegate?.itemDetailViewControllerDidCancel(self)
+    }
+    
+    @IBAction func done() {
+        if let item = itemToEdit {
+            item.text = textField.text!
+            item.shouldRemind = shouldRemindSwitch.isOn
+            item.dueDate = dueDate
+            item.scheduleNotification()
+            delegate?.itemDetailViewController(self,
+                                               didFinishEditing: item)
+        } else {
+            let item = ChecklistItem()
+            item.text = textField.text!
+            item.checked = false
+            // add this
+            item.shouldRemind = shouldRemindSwitch.isOn
+            item.dueDate = dueDate
+            item.scheduleNotification()
+            delegate?.itemDetailViewController(self,
+                                               didFinishAdding: item)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            willSelectRowAt indexPath: IndexPath)
+        -> IndexPath? {
+            if indexPath.section == 1 && indexPath.row == 1 {
+                return indexPath
+            } else {
+                return nil
+            }
+    }
+    
+    override func tableView(_ tableView: UITableView, indentationLevelForRowAt indexPath: IndexPath) -> Int {
+        var newIndexPath = indexPath
+        if indexPath.section == 1 && indexPath.row == 2 {
+            newIndexPath = IndexPath(row: 0, section: indexPath.section)
+        }
+        return super.tableView(tableView, indentationLevelForRowAt: newIndexPath)
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 1 && datePickerVisible {
+            return 3
+        } else {
+            return super.tableView(tableView, numberOfRowsInSection: section)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        textField.resignFirstResponder()
+        if indexPath.section == 1 && indexPath.row == 1 {
+            if !datePickerVisible {
+                showDatePicker()
+            } else {
+                hideDatePicker()
+            }
+        }
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        hideDatePicker()
+    }
+
+    @IBAction func shouldRemindToggle(_ switchControl: UISwitch) {
+        textField.resignFirstResponder()
+        
+        if switchControl.isOn {
+            let center = UNUserNotificationCenter.current()
+            center.requestAuthorization(options: [.alert, .sound]) {
+                granted, error in
+                
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 1 && indexPath.row == 2 {
+            return 217
+        } else {
+            return super.tableView(tableView, heightForRowAt: indexPath)
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        if indexPath.section == 1 && indexPath.row == 2 {
+            return datePickerCell
+        } else {
+            return super.tableView(tableView, cellForRowAt: indexPath)
+        }
+    }
+ 
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Mazboor/View Controllers/SettingTableViewController.swift
@@ -0,0 +1,116 @@
+//
+//  SettingTableViewController.swift
+//  Mazboor
+//
+//  Created by Nima Akbarzade on 11/7/1397 AP.
+//  Copyright © 1397 AP Nima Akbarzade. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewController: UITableViewController {
+    
+    @IBOutlet weak var darkModeSwitch: UISwitch!
+    let defaults = UserDefaults()
+    
+    @IBOutlet var labels: [UILabel]!
+    
+    //    let darkMode = UserDefaults.standard.bool(forKey: "isDarkMode")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if #available(iOS 11.0, *) {
+            navigationController?.navigationBar.prefersLargeTitles = true
+        } else {
+            // Fallback on earlier versions
+        }
+
+//
+//        self.clearsSelectionOnViewWillAppear = false
+//        
+//        darkModeSwitch.isOn = UserDefaults.standard.bool(forKey: "isDarkMode")
+//        
+//        NotificationCenter.default.addObserver(self, selector: #selector(darkModeEnabled(_:)), name: .darkModeEnabled, object: nil)
+//        NotificationCenter.default.addObserver(self, selector: #selector(darkModeDisabled(_:)), name: .darkModeDisabled, object: nil)
+
+        
+    }
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//
+//        UserDefaults.standard.bool(forKey: "isDarkMode") ? enableDarkMode() : disableDarkMode()
+//
+//    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: .darkModeEnabled, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .darkModeDisabled, object: nil)
+    }
+    
+    // MARK: - Theme
+    
+    @objc func darkModeEnabled(_ notification: Notification) {
+        enableDarkMode()
+        self.tableView.reloadData()
+    }
+    
+    @objc func darkModeDisabled(_ notification: Notification) {
+        disableDarkMode()
+        self.tableView.reloadData()
+    }
+
+    open func enableDarkMode() {
+        self.view.backgroundColor = UIColor.black
+        self.tableView.backgroundColor = UIColor.black
+        self.navigationController?.navigationBar.barStyle = .black
+        self.navigationController?.view.backgroundColor = UIColor.black
+    }
+    
+    open func disableDarkMode() {
+        self.view.backgroundColor = UIColor.white
+        self.tableView.backgroundColor = UIColor.white
+        self.navigationController?.navigationBar.barStyle = .default
+        self.navigationController?.view.backgroundColor = UIColor.white
+    }
+    
+    
+    
+    @IBAction func darkMode(_ sender: UISwitch) {
+        
+        UserDefaults.standard.set(sender.isOn, forKey: "isDarkMode")
+        
+        NotificationCenter.default.post(name: sender.isOn ? .darkModeEnabled : .darkModeDisabled, object: nil)
+    }
+    
+    @IBAction func githubButton(_ sender: Any) {
+        if let url = URL(string: "https://github.com/iw4p/Checklist") {
+            UIApplication.shared.open(url, options: [:])
+        }
+    }
+    
+    @IBAction func donateButton(_ sender: Any) {
+        if let url = URL(string: "https://ppng.ir/nimk") {
+            UIApplication.shared.open(url, options: [:])
+        }
+    }
+    
+//    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//
+//        if UserDefaults.standard.bool(forKey: "isDarkMode") {
+//            cell.textLabel?.textColor = UIColor.white
+//            cell.detailTextLabel?.textColor = UIColor.white
+//            cell.backgroundColor = UIColor.black
+//
+////            cell.setSelectedColor(color: UIColor.darkBackground)
+//        }
+//        else {
+//            cell.textLabel?.textColor = UIColor.black
+//            cell.detailTextLabel?.textColor = UIColor.black
+//            cell.backgroundColor = UIColor.white
+//
+////            cell.setSelectedColor(color: UIColor.lightBackground)
+//        }
+//    }
+    
+}
