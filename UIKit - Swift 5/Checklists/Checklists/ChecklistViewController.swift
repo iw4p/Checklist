5d361bc7a62224433c2432201f44482000b561e6
Nima Akbarzade <Nimakbarzade@yahoo.com>
2020-08-06 21:27:29 +0430

    init swift 5 project

---
+++ UIKit - Swift 5/Checklists/Checklists/ChecklistViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Nima Akbarzade on 8/6/20.
+//  Copyright © 2020 padidar. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
