1347c88b94c712e603ba636e6d52c7049357baab
Nima Akbarzade <Nimakbarzade@yahoo.com>
2020-08-07 05:20:17 +0430

    add some info

--- UIKit - Swift 5/Checklists/Checklists/ChecklistViewController.swift
+++ UIKit - Swift 5/Checklists/Checklists/ChecklistViewController.swift
@@ -14,6 +14,32 @@ class ChecklistViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK:- Table View Data Source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row == 1 {
+            label.text = "brush"
+        } else if indexPath.row == 2 {
+            label.text = "learn ios dev"
+        } else if indexPath.row == 3 {
+            label.text = "soccer practice"
+        } else if indexPath.row == 4 {
+            label.text = "eat ice cream"
+        }
+        
+        return cell
+    }
 
 
 }
