39b9ed1949e6ec6a88b70dcd00d2d087fec475db
Nima Akbarzade <Nimakbarzade@yahoo.com>
2020-08-09 23:46:04 +0430

    add empty title handler

--- UIKit - Swift 5/Checklists/Checklists/AddItemViewController.swift
+++ UIKit - Swift 5/Checklists/Checklists/AddItemViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
     
     // MARK:- Outlets
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -21,6 +22,19 @@ class AddItemViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    // MARK:- Text Field Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
     }
 
     // MARK:- Actions
